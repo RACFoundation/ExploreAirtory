055633a12beb8316d1036d4367f66311bb79b7e5
Robin Lovelace <rob00x@gmail.com>
2017-02-25 15:28:05 +0000

    Add app

---
+++ app.R
@@ -0,0 +1,49 @@
+## app.R ##
+library(shinydashboard)
+library(sp)
+source("R/funs.R")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  ## Body content
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(leafletOutput("map1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$map1 <- renderLeaflet({
+    
+    zones <- dl_las()
+    leaflet() %>% addTiles() %>% addPolygons(data = zones)
+  })
+}
+
+shinyApp(ui, server)
