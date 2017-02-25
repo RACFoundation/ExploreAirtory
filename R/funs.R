db6d7ef04e687af0c4dafcb86244b6de1c22884f
wengraf <ivo.wengraf@gmail.com>
2017-02-25 23:12:24 +0000

    altered the shp URLs so that they refer to generalised boundaries roughly as precise as AQMAs

--- R/funs.R
+++ R/funs.R
@@ -1,19 +1,19 @@
 #' @export
 dl_las <- function(zone_type = "las"){
   if(zone_type == "wmc"){
-    url_wmc <- "http://geoportal.statistics.gov.uk/datasets/9507292be10e43c7abe9d3be36db6c41_3.zip"
+    url_wmc <- "http://geoportal.statistics.gov.uk/datasets/9507292be10e43c7abe9d3be36db6c41_0.zip"
     download.file(url_las, destfile = "zones.zip")
     unzip("zones.zip")
-    zones = readOGR("Westminster_Parliamentary_Constituencies_December_2015_Super_Generalised_Clipped_Boundaries_in_Great_Britain.shp")
+    zones = readOGR("Westminster_Parliamentary_Constituencies_December_2015_Full_Clipped_Boundaries_in_Great_Britain.shp")
     f_to_delete <- list.files(pattern = "Westminster_Parliamentary_Constituencies_")
     file.remove(f_to_delete)
     zones
   } 
   if(zone_type == "las"){
-    url_las <- "http://geoportal.statistics.gov.uk/datasets/3943c2114d764294a7c0079c4020d558_4.zip"
+    url_las <- "http://geoportal.statistics.gov.uk/datasets/686603e943f948acaa13fb5d2b0f1275_2.zip"
     download.file(url_las, destfile = "zones.zip")
     unzip("zones.zip")
-    zones = readOGR("Local_Authority_Districts_December_2014_Ultra_Generalised_Clipped_Boundaries_in_Great_Britain.shp")
+    zones = readOGR("Local_Authority_Districts_December_2016_Generalised_Clipped_Boundaries_in_Great_Britain.shp")
     f_to_delete <- list.files(pattern = "Local_Authority_Districts_")
     file.remove(f_to_delete)
     zones
